e72adf043b46be40aa25624e23ed3d60695a05f8
Matt Lilley <thetrime@gmail.com>
2016-09-26 22:07:44 +1300

    Fix build script

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 make distclean
 ./configure ABI=32 --build=i686-pc-darwin "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
-EMCONFIGURE_JS=1 emconfigure ./configure ABI=longlong --build=none --host=none "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+EMCONFIGURE_JS=1 emconfigure ./configure ABI=standard --build=none --disable-assembly CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32
 make clean
 EMMAKEN_CFLAGS="-g" make -j 2
