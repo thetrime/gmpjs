8b8d6c5215c00a30c8be185d8f1f962b92f76645
Matt Lilley <thetrime@gmail.com>
2016-09-27 00:26:18 +1300

    Prevent rebuild each install

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-make distclean
-./configure ABI=32 --build=i686-pc-darwin "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
-EMCONFIGURE_JS=1 emconfigure ./configure ABI=standard --build=none --disable-assembly CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32
-make clean
-EMMAKEN_CFLAGS="-g" make -j 2
+make || make distclean && ./configure ABI=32 --build=i686-pc-darwin "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32" && EMCONFIGURE_JS=1 emconfigure ./configure ABI=standard --build=none --disable-assembly CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32 && make clean && EMMAKEN_CFLAGS="-g" make -j 2
