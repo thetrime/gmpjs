7d1d5faf103f7737552daa38c9ee5d87f11509b3
Matt Lilley <thetrime@gmail.com>
2020-06-02 13:05:31 +0100

    Tidy up build flags for 64 bit hosts

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rebuild_all()
 {
     make distclean || true
-    ./configure ABI=32 --build=i686-pc-darwin "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32" && EMCONFIGURE_JS=1 emconfigure ./configure ABI=standard --build=none --disable-assembly CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32 && make clean && EMMAKEN_CFLAGS="-g" make -j 2
+    EMCONFIGURE_JS=1 emconfigure ./configure CC_FOR_BUILD=gcc CC=emcc ABI=standard --build=none --disable-assembly CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32 && make clean && make -j 2
 }
 
 make || rebuild_all
